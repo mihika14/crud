9118dcc87724d1cc8b249aa1ff8cd15878423155
mihika14 <mihikasaxena13@gmail.com>
2023-06-23 13:04:28 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TableCrud from "./components/TableCrud";
+
+function App() {
+  return (
+    <div className="App">
+      <button>Add data</button>
+     <TableCrud/>
+    </div>
+  );
+}
+
+export default App;
